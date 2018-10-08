bd8da0c757bb7c29c7d00a1dfd874fb488d65b25
Shifath Salam <sheef@Shifaths-MacBook-Pro.local>
2018-10-08 09:06:26 -0700

    project completed

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sheef-player
+A music player using soundcloud api
+
+# ScreenShots
+<img width="1440" alt="screen shot 2018-10-08 at 9 03 03 am" src="https://user-images.githubusercontent.com/43546293/46620301-3a528280-cad9-11e8-9370-3faa1fa338be.png">
+<img width="1440" alt="screen shot 2018-10-08 at 9 03 36 am" src="https://user-images.githubusercontent.com/43546293/46620303-3a528280-cad9-11e8-9b8d-69fa48e1c6cf.png">
--- main.js
+++ main.js
@@ -26,13 +26,24 @@ fetch(`http://api.soundcloud.com/users/${token}${event}`)
 
 function displayResult(data){
 let content = "";
-for(i = 0; i < data.length; i++){
+for(i = 0; i < 8; i++){
   console.log(data[i]);
 
+let id=data[i].id;
+
   content+=`
-    <div class="result-container">
-    <h2>${data[i].username}</h2>
-  </div>`;
+  <div class="card" type="click" value=${id} id=${id} onclick=audio(${id})>
+<img class="card-img-top" src="${data[i].avatar_url}" alt="Card image">
+<div class="card-body">
+  <h4 class="card-title">${data[i].username}</h4>
+  <p>${data[i].first_name}</p>
+</div>
+</div>`;
 }
 display.innerHTML = content;
+}
+
+function audio(id){
+  let audiosrc = document.querySelector("audio")
+  audiosrc.src = "https://api.soundcloud.com/tracks/"+id+"/stream?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f"
 }
